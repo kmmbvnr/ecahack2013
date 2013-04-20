4bedd39d97f07dcb35d04288a9cd8c008c9245c8
Vitalia Solomina <vitalia.podgurskaya@aldigit.com>
2013-04-20 08:41:42 +0400

    Removed title bar

--- FanBurst/src/com/ecahack/fanburst/MainActivity.java
+++ FanBurst/src/com/ecahack/fanburst/MainActivity.java
@@ -11,6 +11,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.util.Log;
 import android.view.Menu;
+import android.view.Window;
 import android.view.WindowManager;
 
 public class MainActivity extends Activity {
@@ -23,6 +24,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		
 		Settings.System.putInt(getContentResolver(),
                 Settings.System.SCREEN_BRIGHTNESS_MODE,
