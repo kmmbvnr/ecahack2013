9fd4bc226dd00e95680f9edf1722911ca7605a45
Vitalia Solomina <vitalia.podgurskaya@aldigit.com>
2013-04-21 13:37:00 +0400

    design improvements

--- FanBurst/src/com/ecahack/fanburst/WSClient.java
+++ FanBurst/src/com/ecahack/fanburst/WSClient.java
@@ -22,7 +22,7 @@ public class WSClient {
 		mClient.connect();
 	}
 	
-	public void sendRegisterInfo(String deviceId, String sector, String row, String place) {
+	public boolean sendRegisterInfo(String deviceId, String sector, String row, String place) {
 		
 		if (validateUserRegisterData(sector, row, place)) {
 			JSONObject object = new JSONObject();
@@ -39,7 +39,9 @@ public class WSClient {
 			}
 			
 			mClient.send(object.toString());
+			return true;
 		}
+		return false;
 	}
 
 	public void sentTimesyncRequest(long currentTimestamp) {
@@ -127,7 +129,9 @@ public class WSClient {
 	}, null);
 	
 	private boolean validateUserRegisterData(String sector, String row, String place) {
-		return (Integer.parseInt(sector) > 0 && Integer.parseInt(row) > 0 && Integer.parseInt(place) > 0);
+		return (sector.length()>0 && Integer.parseInt(sector) > 0 && 
+				row.length()>0 && Integer.parseInt(row) > 0 && 
+				place.length()>0 && Integer.parseInt(place) > 0);
 	}
 
 	
